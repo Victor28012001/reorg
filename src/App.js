4aa9c81fea72189a66ac45f1cfbedc988c7504bd
victor ifeanyi <ifeanyivictor615@gmail.com>
2023-07-03 16:13:05 +0100

    i dey

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          greet victor
-        </a>
-        <h1>hello motherfucker</h1>
-        <h1>hello mother</h1>
-        <h1>hello</h1>
-        <h1>hell</h1>
-      </header>
+      <h1>victor is a bad boy</h1>
     </div>
   );
 }
